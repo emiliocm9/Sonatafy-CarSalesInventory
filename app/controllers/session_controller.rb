162a359e2845bb0c58540722298ebb55f8641de8
Emilio <emilio.contreras97@gmail.com>
2021-06-17 10:56:05 -0500

    docs: session process and view form

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,11 @@
 class SessionController < ApplicationController
     def create
+        @user = User.find_by(name: params[:name])
+        if @user
+           session[:user_id] = @user.id
+           redirect_to cars_path
+        else
+           redirect_to '/'
+        end   
     end
 end
